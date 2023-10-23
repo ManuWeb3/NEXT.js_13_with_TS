3c0f67b5d4d423aa511fcc149e080ce0ccf3ada9
Manu Kapoor <manu.web3.0@gmail.com>
2023-10-23 17:17:07 -0600

    Learned Building APIs

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,16 +1,52 @@
-// GET specific + PUT + PATCH
+// GET (specific) + PUT + PATCH + DELETE
 
 import { NextRequest, NextResponse } from 'next/server'
+import schema from '../schema'
 
 // despite not using "request" object, keep it.
 // if removed, next.js 13 will cache the response
 // next time, we hit the endpoint, it will return the cached response only.
-export function GET(
+// GET takes 2 onjects as input params
+export function GET (
   request: NextRequest,
   { params }: { params: { id: number } }
 ) {
+  // validations on what user sent in the id (no need to check the Req. Body)
   if (params.id > 10)
-    return NextResponse.json({ error: `User With ID: ${params.id} Not Found` })
+    return NextResponse.json({ error: `User With ID: ${params.id} Not Found` }, {status: 404})
 
   return NextResponse.json({ id: 1, name: 'Manu' })
+}
+
+export async function PUT( 
+  request: NextRequest, 
+  {params}:{params: {id: number}}
+  ) {
+    // request => you'll get body
+    // params: params.id => id
+    // any and every validation can be applied onto either/both
+    const body = await request.json()
+    // if(!body.name) 
+    const validation = schema.safeParse(body)
+    if(!validation.success)
+      return NextResponse.json(
+    // {error: "A Valid Name is Required"}, 
+    validation.error.errors,
+    {status: 400}
+    )
+    else if(params.id > 10)
+      return NextResponse.json({error: `User Id ${params.id} Does Not Exist`}, {status: 404})
+    else 
+      return NextResponse.json({id: 1, name: body.name}, {status: 201})
+}
+
+// DELETE = no body, has an id
+export async function DELETE(
+  request: NextRequest, 
+  {params}: {params: {id: number}}
+  ) {
+    // Validation: Check Id
+    if(params.id > 10)
+      return NextResponse.json({error: `User Id ${params.id} Does Not Exist`}, {status: 404})
+    return NextResponse.json({})
 }
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,9 +1,11 @@
-// GET + POST
+// GET (all, Not specific "id") + POST
 
 // no rafec here, bcz no React Component
 // it's a Request Handler fn. this time
 
+// similarly, refactor PUT using zod
 import { NextRequest, NextResponse } from 'next/server'
+import schema from './schema'
 
 // despite not using "request" object, keep it.
 // if removed, next.js 13 will cache the response
@@ -18,15 +20,23 @@ export function GET(request: NextRequest) {
 // inside /users, NOT /[id]
 export async function POST(request: NextRequest) {
   const body = await request.json() // .json() is of JS -> Promise
-  // ideally, real-app, validation of request body (coming form client)
-  // If valid, return data, else error 400 (Client-side code)
+  // ideally, real-app, validation of Request Body (coming form client)
+  // If valid, return data, else error 400 (Client-side code = Bad Request)
   // if a user/client sent a bad data in the request (body), then client-side error = 400
-  if (!body.name)
-    return NextResponse.json(
-      { error: 'Valid name is required' },
-      { status: 400 } // 400 = Bad Request
-    )
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 }) // .json() is of NextJS response -> NO Promise
+  
+  // if (!body.name)
+  const validation = schema.safeParse(body) // parse yells for an error whereas safeParse doesn't + schema test gets applied on "body"
+    if(!validation.success)
+      return NextResponse.json(
+        // { error: 'Valid name is required' }, commented to let zod take care of the errors
+        validation.error.errors,
+        { status: 400 } // 400 = Bad Request
+      )
+    return NextResponse.json({ id: 1, name: body.name }, { status: 201 }) // .json() is of NextJS response -> NO Promise
 }
+  
+/*return NextResponse.json(body)
+}*/
+
 // 201 = Created
 // { id: 1, name: body.name } = JS Object, Not JSON
