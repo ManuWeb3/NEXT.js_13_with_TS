1e9745aa9220588b95789c0bb35b34cc8bde9496
Manu Kapoor <manu.web3.0@gmail.com>
2023-11-07 17:10:44 -0700

    Learning Optimizations in Next.js13

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import ProductCard from './components/ProductCard'
 import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/route'
+import googleImage from '@/public/images/googleImage.png'
 
 // by default, user will be landed at this Home page
 // Home word has nothing to do with this.
@@ -18,6 +19,11 @@ export default async function Home() {
       <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <Link href="./users">Users</Link>
       <ProductCard />
+      <Image src={googleImage} alt="Google"></Image>
+      <Image
+        src="https://www.google.ca/search?q=google+logo&sca_esv=580323760&tbm=isch&sxsrf=AM9HkKmrHzSDqvxLXViAhIR8DSDuva_IdQ%3A1699409704067&source=hp&biw=1536&bih=695&ei=J-9KZdLNN73k0PEP5cOiwAU&iflsig=AO6bgOgAAAAAZUr9ODkDxhk58uQSBBL98sxlwbuAEaB3&ved=0ahUKEwiS_eq9qrOCAxU9MjQIHeWhCFgQ4dUDCAc&uact=5&oq=google+logo&gs_lp=EgNpbWciC2dvb2dsZSBsb2dvMggQABiABBixAzIIEAAYgAQYsQMyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABEjDI1D5B1itIHACeACQAQCYAdIBoAHuCaoBBTguMy4xuAEDyAEA-AEBigILZ3dzLXdpei1pbWeoAgrCAgcQIxjqAhgnwgIEECMYJw&sclient=img#imgrc=2FEMalISLOerzM"
+        alt="Google Logo"
+      ></Image>
     </main>
   )
 }
